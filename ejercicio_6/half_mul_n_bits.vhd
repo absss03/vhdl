ac54067aae3fa30c5de9842583b378a7ae577167
absss03 <barbieriabigailok@gmail.com>
2026-09-09 19:39:41 -0300

    Update: Subo ej 6 sin testbench y sin revisar

---
+++ ejercicio_6/half_mul_n_bits.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity half_mul_n is
+  generic (
+    N : integer := 8
+  );
+  port (
+    a_i, b_i : in  std_logic_vector(N-1 downto 0);
+    y_o      : out std_logic_vector(N-1 downto 0)
+  );
+end half_mul_n;
+
+architecture rtl of half_mul_n is
+begin
+  y_o <= std_logic_vector(unsigned(a_i) * unsigned(b_i));
+end architecture rtl;
