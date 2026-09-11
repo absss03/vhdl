f5173fd7d766af9ce00b6336293e671297d007a9
absss03 <barbieriabigailok@gmail.com>
2026-09-11 12:01:21 -0300

    Update: Subo ej 15 sin testbench

---
+++ ejercicio_15/shift_reg_load_n.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity shift_reg_load is
+  generic (
+    N : integer := 8
+  );
+  port (
+    clk , rst, load, d_i: in std_logic;
+		vector_i : in std_logic_vector(N-1 downto 0);
+    y_o : out std_logic_vector(N-1 downto 0)
+  );
+end shift_reg_load;
+
+architecture rtl of shift_reg_load is
+  signal shift : std_logic_vector(N-1 downto 0);
+begin
+  label : process(clk, rst)
+  begin
+    if rst = '0' then
+      shift <= (others => '0');
+    elsif rising_edge(clk) then
+			if load = '1' then
+				shift <= vector_i;
+      else
+      	shift <= shift(N-2 downto 0) & d_i;
+			end if;
+    end if;
+  end process;
+  y_o <= shift;
+end architecture rtl;
